7aab5f78fcb2f7eedbf1e14e28faae02107f750d
Julia Aasmaa <my_e_10@hotmail.com>
2021-09-15 11:44:45 +0300

    exercise

---
+++ WeatherReportSwitch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherReportSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Week3Exercises/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week3Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tingimused sisseastumiseks:
+            //miinimum matemaatikas 85 punkti;
+            //miinimum keemias 90 punkti;
+            //miinimum bioloogias 95 punkti;
+            //programm küsib kasutajal sisestada tema 
+            //eksami punktid ja otsustab, kas kasutaja
+            //saab arstiks õppida
+
+            Console.WriteLine("Sisesta matemaatika eksami tulemus:");
+            int math = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+}
